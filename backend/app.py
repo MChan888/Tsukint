df5a2d2b9b8a3c19bb231a4863b84768b28c4415
Camilo Orlandis <corlandis@fi.uba.ar>
2024-07-28 19:18:57 -0300

    Feat: primer endpoint y tablas

--- backend/app.py
+++ backend/app.py
@@ -1,28 +1,43 @@
-from flask import Flask, send_from_directory, jsonify, insert
-from models import db, Player, Weapon, Collection, Skin
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-port = 5000
-app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://postgres:1234@localhost:5432/test'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://postgres:1234@localhost:5432/postgres'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    with app.app_context():
+        from models import Weapon
+        db.create_all()
+
+        weapons = [
+            {"wName": "AWP", "wType": "scopedRifle", "wPrice": 4750, "wOrigin": "UK"},
+            {"wName": "M4A4", "wType": "rifle", "wPrice": 3100, "wOrigin": "US"},
+            {"wName": "AK47", "wType": "rifle", "wPrice": 2700, "wOrigin": "RUS"},
+            {"wName": "Karambit", "wType": "melee", "wPrice": 9999, "wOrigin": "ARG"},
+            {"wName": "MP9", "wType": "submachinegun", "wPrice": 1350, "wOrigin": "US"},
+        ]
+
+        for weapon in weapons:
+            exists = Weapon.query.filter_by(wName=weapon['wName'], wType=weapon['wType'], wPrice=weapon['wPrice'], wOrigin=weapon['wOrigin']).first()
+            if not exists:
+                db.session.add(Weapon(**weapon))
+
+        db.session.commit()
+    @app.route('/api/weapons', methods=['GET'])
+    def get_weapons():
+        weapons = Weapon.query.all()
+        weapons_list = [{"wId": weapon.wId, "wName": weapon.wName, "wType": weapon.wType, "wPrice": weapon.wPrice, "wOrigin": weapon.wOrigin} for weapon in weapons]
+        return jsonify(weapons_list)
+
+    return app
 
 
-@app.route('/', methods = ["POST"])
-def add_player(player):
-    return 
-
-@app.route('/', methods = ["GET"])
-def show_players():
-    players = db.query('SELECT * FROM players')
-    return jsonify(players)
-
-@app.route('/', methods = ["PUT"])
-def update_player():
-    return
-
-@app.route('/', methods = ["DELETE"])
-def delete_player():
-    return
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app = create_app()
+    app.run(port=8080, debug=True)
--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,8 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from app import db
 
 class Player(db.Model):
     __tablename__ = 'players'
-    pId = db.Column(db.Integer, primary_key=True)
+    pId = db.Column(db.Integer, primary_key=True, autoincrement=True)
     pName = db.Column(db.String, nullable=False)
     pType = db.Column(db.String, nullable=False)
     pOrigin = db.Column(db.String, nullable=False)
@@ -12,22 +10,23 @@ class Player(db.Model):
 
 class Weapon(db.Model):
     __tablename__ = 'weapons'
-    wId = db.Column(db.Integer, primary_key=True)
+    wId = db.Column(db.Integer, primary_key=True, autoincrement=True)
     wName = db.Column(db.String, nullable=False)
+    wPrice = db.Column(db.Integer, nullable=False)
     wType = db.Column(db.String, nullable=False)
     wOrigin = db.Column(db.String, nullable=False)
 
-class Collection():
-    __talbename__ = 'collections'
-    cId = db.Column(db.Integer, primary_key=True)
+class Collection(db.Model):
+    __tablename__ = 'collections'
+    cId = db.Column(db.Integer, primary_key=True, autoincrement=True)
     cName = db.Column(db.String, nullable=False)
 
 class Skin(db.Model):
-    __tablename__ = 'players'
-    sId = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'skins'
+    sId = db.Column(db.Integer, primary_key=True, autoincrement=True)
     sName = db.Column(db.String, nullable=False)
-    wId = db.Column(db.String, db.ForeignKey('weapons.id'))
+    wId = db.Column(db.Integer, db.ForeignKey('weapons.wId'))
     sMPrice = db.Column(db.Integer, nullable=False)
     sFloat = db.Column(db.String, nullable=False)
-    pId = db.Column(db.Integer, db.ForeignKey('player.id'), nullable=False)
-    cId = db.Column(db.Integer, db.ForeignKey('collection.id'), nullable=False)
+    pId = db.Column(db.Integer, db.ForeignKey('players.pId'))
+    cId = db.Column(db.Integer, db.ForeignKey('collections.cId'))

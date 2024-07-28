6085f55e60094a7741b061359b90d9a35349dcf8
Camilo Orlandis <corlandis@fi.uba.ar>
2024-07-28 17:50:41 -0300

    Se agregan tablas y endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, send_from_directory
-from models import db, Player, Weapon, Skin
+from flask import Flask, send_from_directory, jsonify, insert
+from models import db, Player, Weapon, Collection, Skin
 
 app = Flask(__name__)
 port = 5000
@@ -7,37 +7,22 @@ app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://postgres:1234@loca
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 
-
-@app.route('/frontend', methods = ["POST"])
+@app.route('/', methods = ["POST"])
 def add_player(player):
-    db.session.add(player)
-    db.session.commit()
     return 
 
-@app.route('/frontend', methods = ["GET"])
+@app.route('/', methods = ["GET"])
 def show_players():
-    return
+    players = db.query('SELECT * FROM players')
+    return jsonify(players)
 
-@app.route('/frontend', methods = ["PUT"])
+@app.route('/', methods = ["PUT"])
 def update_player():
     return
 
-@app.route('/frontend', methods = ["DELETE"])
+@app.route('/', methods = ["DELETE"])
 def delete_player():
-    db.session.delete(player)
     return
 
-@app.route('/front/player')
-def player():
-    return send_from_directory(app.front + '/player', 'index.html')
-
-@app.route('/front/skins')
-def skins():
-    return send_from_directory(app.front + '/skins', 'index.html')
-
-@app.route('/front/weapons')
-def weapons():
-    return send_from_directory(app.front + '/weapons', 'index.html')
-
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

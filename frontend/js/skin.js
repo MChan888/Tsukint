2d4b24ac566211b421cb20350189e8061db00b11
Camilo Orlandis <corlandis@fi.uba.ar>
2024-07-28 23:08:36 -0300

    Fix: Headers

--- frontend/js/skin.js
+++ frontend/js/skin.js
@@ -9,7 +9,7 @@ const createSkinmodel = async (name, type) => {
 	await fetch("http://localhost:5000/api/skins/model", {
 		method: "POST",
 		headers: {
-			"Content-Type": "application/json",
+			"Content-Type": "application/json"
 		},
 		body: JSON.stringify({
 			modelName: name,

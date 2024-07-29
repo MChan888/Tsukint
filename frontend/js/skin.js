7c0e697ad85a33c539489d208dfa735a9197ebd9
MChan888 <mchan@fi.uba.ar>
2024-07-28 22:44:47 -0300

    Feat: Creacion de skins

--- frontend/js/skin.js
+++ frontend/js/skin.js
@@ -4,3 +4,30 @@ const changeSkinBoxView = () => {
 		element.removeChild;
 	}
 };
+
+const createSkinmodel = async (name, type) => {
+	await fetch("/api/skins/model", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			wName: name,
+			wId: type,
+		}),
+	})
+		.then((response) => response.json())
+		.then((data) => console.log(data))
+		.catch((error) => console.error("Error:", error));
+};
+
+const test = () => {
+	const name = document.getElementById("wName").value;
+	const type = document.getElementById("wType").value;
+
+	if (!name || !type) {
+		return alert("No dejes los espacios en blanco!");
+	}
+
+	return createSkinmodel(name, type);
+};
